dae3bb8bdbf23eaa354920575bb9fbb47f406661
tunde fabiyi <tunde fabiyi>
2019-02-03 20:42:09 +0100

    .

--- app/server.ts
+++ app/server.ts
@@ -5,20 +5,19 @@ import * as cors from "cors";
 import { initHandlers } from "./eventhandlers/index";
 import { initRoutes } from "./routes/index";
 import * as socketio from "socket.io";
+import * as config from "config";
 const PORT = 3890;
 const mongoUrl: string = "mongodb://localhost/kpashidatabase";
+
 var app = express();
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cors({ credentials: true, origin: config.get("clientaddress") }));
 // initHandlers(this.app);
 app.use(bodyParser.json());
 //support application/x-www-form-urlencoded post data
 app.use(bodyParser.urlencoded({ extended: false }));
 initRoutes(app);
 
-mongoose.connect(
-  mongoUrl,
-  { useNewUrlParser: true }
-);
+mongoose.connect(config.get("dbconstring"), { useNewUrlParser: true });
 
 var server = app.listen(PORT, () => {
   console.log("Express server listening on port... " + PORT);
--- dist/server.js
+++ dist/server.js
@@ -7,16 +7,17 @@ const cors = require("cors");
 const index_1 = require("./eventhandlers/index");
 const index_2 = require("./routes/index");
 const socketio = require("socket.io");
+const config = require("config");
 const PORT = 3890;
 const mongoUrl = "mongodb://localhost/kpashidatabase";
 var app = express();
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cors({ credentials: true, origin: config.get("clientaddress") }));
 // initHandlers(this.app);
 app.use(bodyParser.json());
 //support application/x-www-form-urlencoded post data
 app.use(bodyParser.urlencoded({ extended: false }));
 index_2.initRoutes(app);
-mongoose.connect(mongoUrl, { useNewUrlParser: true });
+mongoose.connect(config.get("dbconstring"), { useNewUrlParser: true });
 var server = app.listen(PORT, () => {
     console.log("Express server listening on port... " + PORT);
 });
