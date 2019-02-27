9783fed8c67da5bbc8824a6eb6a5c2ecf409f985
tunde fabiyi <tunde fabiyi>
2019-02-27 03:42:56 +0100

    .

--- app/commands/registration/sendTableInvite.ts
+++ app/commands/registration/sendTableInvite.ts
@@ -1,10 +1,16 @@
 import { KpashiTable } from "../../domain/kpashiTable";
+import * as mongoose from "mongoose";
 import { getKpashiTable } from "../../repositories/kpashiTableRepo";
 import { getTableInfo } from "../../queries/getTableInfo";
 import { getUserInfo } from "../../queries/getUserInfo";
 import * as postal from "postal";
+import { kpashiOnlineTrackerInfoSchema } from "../../models/kpashiOnlineTrackerInfo";
 
 import { postalChannels, postalTopics } from "../../param";
+const kpashiOnlineTrackerInfo = mongoose.model(
+  "kpashiOnlineTrackerInfo",
+  kpashiOnlineTrackerInfoSchema
+);
 export class sendTableInvite {
   hostuserid: string;
   guestuserid: string;
@@ -21,6 +27,11 @@ export class sendTableInvite {
 
     if (kpashitable.playerAlreadyExist(this.guestuserid))
       throw "Player Already In Group";
+    var onlinerec = await kpashiOnlineTrackerInfo.findOne({
+      userid: this.guestuserid
+    });
+    if (onlinerec == null || onlinerec == undefined)
+      throw "this player is no more online";
     var eventobj: any = {};
     eventobj.tableinfo = await getTableInfo(this.tableid);
     eventobj.hostuserinfo = await getUserInfo(this.hostuserid);
